a057a560e81b3d4cef69f149823e28bedfce323f
Ricardo <1151888@isep.ipp.pt>
2019-01-06 11:49:57 +0000

    configs

--- web.js
+++ web.js
@@ -1,5 +1,10 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
 console.log(process.env.PORT || 5000);
 app.listen(process.env.PORT || 5000);
